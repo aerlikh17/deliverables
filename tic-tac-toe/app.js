049d19341bbaea27ef3a0e8113be232836d1d413
aerlikh <aerlikh2011@gmail.com>
2022-10-03 17:19:13 -0400

    final version

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,6 +1,4 @@
-
-
-// Constants. Winning combos:
+// Constants
 const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -12,69 +10,76 @@ const winConditions = [
     [2, 4, 6]
 ];
 
+// Cached elements
+const cells = document.querySelectorAll(".cell");
+const statusText = document.getElementById("statusText");
+const restartBtn = document.querySelector("#restartBtn");
+
  // Variables
 let options = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
 let running = false;
 
-// Cached elements
-
-const cells = document.querySelectorAll(".cell");
-const statusText = document.querySelector("#statusText");
-const restartBtn = document.querySelector("#restartBtn");
-
-
 // Functions
 
-initializeGame();
+//First, we need to initiliaze the game by calling initialize() function
+initialize();
 
-function initializeGame(){
+//First, we add an event listener to every cell which is a "click". Our callback is function cellClicked. 
+//Then, we add an event listener to our button and we callback restartGame function.
+//Finally, third line is for the message and we are checking if game is running;
+
+function initialize(){
     cells.forEach(cell => cell.addEventListener("click", cellClicked));
     restartBtn.addEventListener("click", restartGame);
     statusText.textContent = `${currentPlayer}'s turn`;
     running = true;
 }
+
+//This function is what happens when the cell is clicked on;
 function cellClicked(){
-    const cellIndex = this.getAttribute("cellIndex");
-
+    const cellIndex = this.getAttribute("cellIndex"); //First, we create a local variable cellIndex. "This" refers to whatever cell that's been clicked on.
+    // getAttribute is using "cellIndex" to get cell's index. 
     if(options[cellIndex] != "" || !running){
-        return;
+        return; // nothing will happen
     }
-
     updateCell(this, cellIndex);
     checkWinner();
 }
+
 function updateCell(cell, index){
     options[index] = currentPlayer;
-    cell.textContent = currentPlayer;
+    cell.textContent = currentPlayer; 
 }
+
 function changePlayer(){
-    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    currentPlayer = (currentPlayer === "X") ? "O" : "X"; //if current player is X, we will reassign to O. Otherwise, X. 
     statusText.textContent = `${currentPlayer}'s turn`;
 }
+
 function checkWinner(){
     let roundWon = false;
 
     for(let i = 0; i < winConditions.length; i++){
         const condition = winConditions[i];
-        const cellA = options[condition[0]];
-        const cellB = options[condition[1]];
+        const cellA = options[condition[0]]; //we are iterating over const winConditions array, there are 3 indices in each array;
+        const cellB = options[condition[1]]; //if there are no spaces, and they are all the same character, somebody won.
         const cellC = options[condition[2]];
 
-        if(cellA == "" || cellB == "" || cellC == ""){
+        if(cellA === "" || cellB === "" || cellC === ""){ //if there are no empty spaces, we continue iterating through arrays.
             continue;
         }
-        if(cellA == cellB && cellB == cellC){
+        if(cellA === cellB && cellB === cellC){ //making sure all the full spaces are the same character.
             roundWon = true;
             break;
         }
     }
 
-    if(roundWon){
+    if(roundWon){ //if roundWon = true;
         statusText.textContent = `Player ${currentPlayer} wins!`;
         running = false;
     }
-    else if(!options.includes("")){
+    else if(!options.includes("")){ //if there are no spaces left, it is a draw.
         statusText.textContent = `Draw! Press "PLAY" to play again`;
         running = false;
     }
@@ -82,6 +87,7 @@ function checkWinner(){
         changePlayer();
     }
 }
+
 function restartGame(){
     currentPlayer = "X";
     options = ["", "", "", "", "", "", "", "", ""];
