b42c387d8ca731c03f49d891c0f813fdea7afa3d
aerlikh <aerlikh2011@gmail.com>
2022-10-19 16:43:39 -0400

    updates

--- mongoose-flights/controllers/flights.js
+++ mongoose-flights/controllers/flights.js
@@ -3,11 +3,16 @@ const Flight = require('../models/flight');
 module.exports = {
     new: newFlight,
     create,
-    index
+    index,
+    dynamicSort
 };
 
 function newFlight(req, res) {
-    res.render('flights/new');
+    const newFlight = new Flight();
+    const dt = newFlight.departs;
+    const departsDate = dt.toISOString().slice(0, 16);
+
+    res.render('flights/new', {departsDate});
 };
 
 function create(req, res) {
@@ -16,12 +21,26 @@ function create(req, res) {
     flight.save(function(err){
         if (err) return res.send('There was an error');
         console.log(flight);
-        res.redirect('/flights/new');
+        res.redirect('/flights');
     })
 };
 
-function index(req, res) {
-    Flight.find({}, function(err, flights) {
-        res.render('flights/index', { title: 'All Flights', flights });
-      });
+function dynamicSort(property) {
+    let sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
 }
+
+function index(req, res) {
+    Flight.find({}, function(err, flights){
+    flights.sort(dynamicSort('-departs'))
+        if (err) res.send(err.message);
+        res.render('flights/index.ejs', { flights });
+    }
+)};
